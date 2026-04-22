36167f4cf27d72d383f7724d13942b2c613794b1
Emilse Arias <emilsearias@gmail.com>
2026-04-22 20:26:46 -0300

    agregamos los estilos del navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import { LuGithub } from "react-icons/lu";
 
 const Navbar = () => {
   return (
-    <nav>
-      <span>Movies CRD</span>
-      <a href="#">icono{/* <LuGithub /> */}</a>
+    <nav className="bg-slate-900 p-8 flex items-center justify-between">
+      <span className="text-xl font-extrabold">Movies CRD</span>
+      <a
+        href="#"
+        className="bg-black p-2.5 rounded-lg hover:bg-white hover:text-black transition-colors duration-300"
+      >
+        <LuGithub className="size-6" />
+      </a>
     </nav>
   );
 };

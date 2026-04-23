340ece94597bd56ea38cb32e9d43ab3b1ea124e7
Emilse Arias <emilsearias@gmail.com>
2026-04-22 21:25:44 -0300

    agregamos estilos a la grilla para hacerlo responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ function App() {
   return (
     <>
       <Navbar />
-      <main className="grow">
+      <main className="grow container mx-auto my-5">
+        <h1 className="text-center text-2xl font-bold my-4">Administrar Peliculas</h1>
         <FormMovie></FormMovie>
       </main>
       <Footer />
--- src/components/GridMovie.jsx
+++ src/components/GridMovie.jsx
@@ -4,7 +4,10 @@ import CardMovie from "./CardMovie";
 const GridMovie = ({movies}) => {
   return (
     // grid-cols-1 (móvil), sm:grid-cols-2 (tablet), lg:grid-cols-4 (desktop)
-    <div >
+    <div className="mt-4 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <CardMovie></CardMovie>    
+      <CardMovie></CardMovie>    
+      <CardMovie></CardMovie>    
       <CardMovie></CardMovie>    
     </div>
   );
